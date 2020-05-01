1a10dcad10ac65bcd3968a967ab9e46ec99cfdec
zhykirby <zhykirby@qq.com>
2020-05-02 00:05:26 +0800

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,7 @@
 const router = require('koa-router')()
 const controller = require('../controller/index')
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+
 
 //example
 /**
